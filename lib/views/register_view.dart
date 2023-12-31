26595336c0bebac22fca0ce462c57b7c3da64604
Tejaswa Bedi <tejaswa.20b1541008@abes.ac.in>
2024-01-01 00:32:28 +0530

    "Verify email added"

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -84,6 +84,12 @@ class _RegisterViewState extends State<RegisterView> {
                       },
                       child: const Text("Register"),
                     ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamedAndRemoveUntil(
+                              "/login/", (route) => false);
+                        },
+                        child: const Text("Already registerd ? Login Here"))
                   ],
                 );
               default:
